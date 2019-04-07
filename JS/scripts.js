47086b00eb01f0fbb72fba20a96c21ea22c44548
naisenyamungai <naisenyamungai@gmail.com>
2019-04-07 20:46:06 +0300

    update styles.css, scripts.js and index.html

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,5 +6,20 @@ function openNav() {
     document.getElementById("myNav").style.width = "0%";
   }
 
+  $(document).ready(function() {
+    $(".clickable1").click(function() {
+      $(".modal1").toggle();
+    });
+  });
 
+  $(document).ready(function() {
+    $(".clickable2").click(function() {
+      $(".modal2").toggle();
+    });
+  });
+  
+
+  
+
+ 
 
